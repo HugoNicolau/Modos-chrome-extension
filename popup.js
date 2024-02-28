2d318540eace9677391846f0461d3dd466670751
HugoNicolau <nicolau.hugogiles@gmail.com>
2024-02-28 16:55:28 -0300

    Popup adjust for play/pause extension

--- popup.js
+++ popup.js
@@ -3,19 +3,14 @@ document.addEventListener('DOMContentLoaded', function() {
 
     toggleButton.addEventListener('click', function() {
         // Get the current flag value
-        chrome.storage.local.get('contentEnabled', function(data) {
-            const contentEnabled = data.contentEnabled;
-            // Toggle the flag
-            const newContentEnabled = !contentEnabled;
-            chrome.storage.local.set({ 'contentEnabled': newContentEnabled }, function() {
-                console.log('Content script enabled:', newContentEnabled);
-                toggleButton.textContent = newContentEnabled ? 'Disable Content' : 'Enable Content';
+        chrome.storage.local.get('extensionPaused', function(data) {
+            const extensionPaused = data.extensionPaused;
+            const newExtensionPaused = !extensionPaused;
+            chrome.storage.local.set({ 'extensionPaused': newExtensionPaused }, function() {
+                console.log('Extension state changed:', newExtensionPaused ? 'paused' : 'playing');
+                toggleButton.textContent = newExtensionPaused ? 'Play Extension' : 'Pause Extension';
 
-
-                chrome.tabs.query({ active: true, currentWindow: true}, function(tabs) {
-                    const currentTab = tabs[0];
-                    chrome.tabs.sendMessage(currentTab.id, { action: 'toggleScript', contentEnabled: newContentEnabled});
-                });
+                chrome.runtime.sendMessage({ action: 'toggleExtensionState', extensionPaused: newExtensionPaused });
             });
         });
     });
