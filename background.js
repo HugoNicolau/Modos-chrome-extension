fd1a8d2fc62091b0e1c357d88641ab1ae7128de7
HugoNicolau <nicolau.hugogiles@gmail.com>
2024-02-28 16:51:09 -0300

    Flag to pause/play extension

--- background.js
+++ background.js
@@ -1,24 +1,13 @@
-
-  // chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-
-  //   if (message.requestUrl) {
-  //     let currentUrl = getCurrentUrl();
-  //     chrome.runtime.sendMessage({url: currentUrl})
-
-  //     console.log("URL visitado:", url)
-  //   }
-  // });
-
-
-  setInterval(function() {
-    let currentUrl = getCurrentUrl();
-    chrome.storage.local.get('visitedSites', function(data) {
-      let visitedSites = data.visitedSites || [];
-      visitedSites.push(currentUrl);
-      chrome.storage.local.set({ 'visitedSites': visitedSites });
+// Initialize the extension state
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.storage.local.set({ 'extensionPaused': false });
+});
+// Listen for messages from the popup script to toggle the extension state
+chrome.runtimeonMessage.addListener(function(message, sender, sendResponse) {
+  if (message.action === 'toggleExtensionState') {
+    const extensionPaused = message.extensionPaused;
+    chrome.storage.local.set({ 'extensionPaused': extensionPaused }, function() {
+      console.log('Extension state changed:', extensionPaused ? 'paused' : 'playing');
     });
-  }, 10000);
-
-  function getCurrentUrl() {
-    return window.location.href;
   }
+});
