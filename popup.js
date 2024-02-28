efde5af145ae4432667ef2a06ced57b016e8fe54
HugoNicolau <nicolau.hugogiles@gmail.com>
2024-02-28 17:28:29 -0300

    popup configuratin to match extension state

--- popup.js
+++ popup.js
@@ -1,17 +1,35 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const toggleButton = document.getElementById('toggleButton');
+// Function to update the UI based on the stored state
+function updatePopupUI(state) {
+  const toggleButton = document.getElementById("toggleButton");
+  toggleButton.textContent = state ? "Pause Extension" : "Play Extension";
+}
 
-    toggleButton.addEventListener('click', function() {
-        // Get the current flag value
-        chrome.storage.local.get('extensionPaused', function(data) {
-            const extensionPaused = data.extensionPaused;
-            const newExtensionPaused = !extensionPaused;
-            chrome.storage.local.set({ 'extensionPaused': newExtensionPaused }, function() {
-                console.log('Extension state changed:', newExtensionPaused ? 'paused' : 'playing');
-                toggleButton.textContent = newExtensionPaused ? 'Play Extension' : 'Pause Extension';
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleButton = document.getElementById("toggleButton");
 
-                chrome.runtime.sendMessage({ action: 'toggleExtensionState', extensionPaused: newExtensionPaused });
-            });
-        });
+  // Load the initial state from chrome.storage.local and update de UI
+  chrome.storage.local.get("extensionPaused", function (data) {
+    const extensionPaused = data.extensionPaused;
+    updatePopupUI(extensionPaused);
+  });
+
+  // Listen for button click to toggle the extension state
+  toggleButton.addEventListener("click", function () {
+    chrome.storage.local.get("extensionPaused", function (data) {
+      const extensionPaused = data.extensionPaused;
+      const newExtensionPaused = !extensionPaused;
+
+      chrome.storage.local.set(
+        { extensionPaused: newExtensionPaused },
+        function () {
+          console.log(
+            "Extension state changed:",
+            newExtensionPaused ? "paused" : "playing"
+          );
+          // Update the UI
+          updatePopupUI(newExtensionPaused);
+        }
+      );
     });
+  });
 });
